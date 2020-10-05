379faef1382f3a70c8a9c11ea616bddc72833c85
Siddhant Grover <sgrover@xpanxion.com>
2020-10-05 17:45:35 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;  
+using System.Linq;  
+using System.Threading.Tasks;  
+using Microsoft.AspNetCore.Mvc;
+using ReactCrudDemo.Models;
+using ReactCrudDemo1.Models;  
+
+
+namespace ReactCrudDemo1.Controllers
+{  
+    public class EmployeeController : Controller  
+    {  
+        EmployeeDataAccessLayer objemployee = new EmployeeDataAccessLayer();  
+        
+        [HttpGet]  
+        [Route("api/Employee/Index")]  
+        public IEnumerable<TblEmployee> Index()
+        {  
+            return objemployee.GetAllEmployees();  
+        }  
+        
+        [HttpPost]  
+        [Route("api/Employee/Create")]  
+        public int Create(TblEmployee employee)
+        {  
+            return objemployee.AddEmployee(employee);  
+        }
+
+        [HttpGet]
+        [Route("api/Employee/Details/{id}")]
+        public TblEmployee Details(int id)
+        {
+            return objemployee.GetEmployeeData(id);
+        }
+
+
+        [HttpPut]
+        [Route("api/Employee/Edit")]
+        public int Edit(TblEmployee employee)
+        {
+            return objemployee.UpdateEmployee(employee);
+        }
+
+        [HttpDelete]
+        [Route("api/Employee/Delete/{id}")]
+        public int Delete(int id)
+        {
+            return objemployee.DeleteEmployee(id);
+        }
+
+        [HttpGet]
+        [Route("api/Employee/GetCityList")]
+        public IEnumerable<TblCities> Details()
+        {
+            return objemployee.GetCities();
+        }
+    }  
+
+}
---
+++ Models/EmployeeContext.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ReactCrudDemo.Models
+{
+    public partial class EmployeeContext : DbContext
+    {
+        public EmployeeContext()
+        {
+        }
+
+        public EmployeeContext(DbContextOptions<EmployeeContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblCities> TblCities { get; set; }
+        public virtual DbSet<TblEmployee> TblEmployee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=.\\sqlexpress;Initial Catalog=Employee;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TblCities>(entity =>
+            {
+                entity.HasKey(e => e.CityId)
+                    .HasName("PK__tblCitie__F2D21A96EE3BFB13");
+
+                entity.ToTable("tblCities");
+
+                entity.Property(e => e.CityId).HasColumnName("CityID");
+
+                entity.Property(e => e.CityName)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TblEmployee>(entity =>
+            {
+                entity.HasKey(e => e.EmployeeId)
+                    .HasName("PK__tblEmplo__7AD04FF15C918CDC");
+
+                entity.ToTable("tblEmployee");
+
+                entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Department)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Gender)
+                    .IsRequired()
+                    .HasMaxLength(6)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
